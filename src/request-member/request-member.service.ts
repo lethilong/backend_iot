78ee5cc10c71221b3824aaff0e0a32c164d15b24
lethilong <lelong090801@gmail.com>
2023-03-06 11:09:31 +0700

    fix: sort request and noti by time created

--- src/request-member/request-member.service.ts
+++ src/request-member/request-member.service.ts
@@ -29,7 +29,7 @@ export class RequestMemberService {
     async getRequests(id, data: GetRequestsDto) {
         const { pageSize = 10, pageNumber = 1, status } = data;
         const findOption = status ? { to: id, status: status } : { to: id }
-        const requests = await this.requestMemberModel.find(findOption).select('from home status').skip(pageSize * (pageNumber - 1)).limit(pageSize).populate('from', 'name phone').populate('home', 'name address');
+        const requests = await this.requestMemberModel.find(findOption).select('from home status createdAt').skip(pageSize * (pageNumber - 1)).limit(pageSize).populate('from', 'name phone').populate('home', 'name address').sort({ 'createdAt': -1 });
         const total = await this.requestMemberModel.count(findOption);
         return new ConfirmResponse({
             data: {
